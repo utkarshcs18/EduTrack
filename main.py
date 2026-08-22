868d94af0f80b6296db71c45536869e358d9633d
utkarshcs18 <utkarshkumar.cs18@gmail.com>
2026-08-22 19:47:37 +0530

    add

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv("studentData.csv", encoding="latin1")
+
+print(f'First 5 rows-> \n{df.head()}')
+print(f'Last 5 rows-> \n{df.tail()}')
+
+print(df)
+
+row ,column = df.shape
+
+print(f'Rows-> {row}')
+print(f'Column-> {column}')
+
+print("Column name : ")
+print(df.columns)
+
+print(df.info())
+
